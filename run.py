b2a8071c2569d1b9d88202a478c2a40049f860c7
Day <dayannefernandesc@gmail.com>
2016-12-12 09:29:28 -0200

    test js navbar

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
 from meninascomp import app
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from meninascomp.meninascomp import app
+from meninascomp import app
 
 class TestIndex(unittest.TestCase):
 
